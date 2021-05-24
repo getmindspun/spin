34b9c18880fbfab671eda9a3b148dcd65927e9d3
Mindspun <mindspun@mindspun.com>
2021-05-24 10:59:28 -0700

    Add the 'forms' setup function to main.js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ gtu.ready(function () {
     'use strict';
     search();
     buttons();
+    forms();
 });
 
 function search() {
@@ -307,4 +308,39 @@ function buttons() {
         color: '#ffffff',
         hoverColor: '#ffffff'
     });
+}
+
+function forms() {
+    /*
+     * Enable forms that have a data-form-type attribute set to 'contact' by:
+     * - append a hidden form element containing the customer api key (if not present).
+     * - set the method attribute to 'POST'
+     * - set the action to the /form endpoint
+     * - set the redirect location (if not already present)
+     */
+    var formElements = document.querySelectorAll('form[data-form-type="contact"]');
+    if (formElements.length > 0) {
+        for (var i = 0; i < formElements.length; i++) {
+            var formElement = formElements[i];
+
+            if (!formElement.querySelector('input[name="api_key"]')) {
+                var hiddenInputElement = document.createElement('input');
+                hiddenInputElement.setAttribute('hidden', 'true');
+                hiddenInputElement.setAttribute('name', 'api_key');
+                hiddenInputElement.setAttribute('value', 'API_KEY');
+                formElement.appendChild(hiddenInputElement);
+            }
+
+            if (!formElement.querySelector('input[name="location"]')) {
+                var locationInputElement = document.createElement('input');
+                locationInputElement.setAttribute('hidden', 'true');
+                locationInputElement.setAttribute('name', 'location');
+                locationInputElement.setAttribute('value', './#thank-you');
+                formElement.appendChild(locationInputElement);
+            }
+
+            formElement.setAttribute('method', 'POST');
+            formElement.setAttribute('action', 'https://api.mindspun.com/form');
+        }
+    }
 }
